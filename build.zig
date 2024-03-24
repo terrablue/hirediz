c26f4010aadfe9d49be08e86e90589ef6036f922
terrablue <102580937+terrablue@users.noreply.github.com>
2024-03-24 01:27:30 +0100

    fix install header

--- build.zig
+++ build.zig
@@ -20,7 +20,7 @@ pub fn build(b: *std.build.Builder) void {
 
     lib.installHeader("hiredis.h", "hiredis.h");
     lib.installHeader("read.h", "read.h");
-    lib.installHeader("sdh.h", "sdh.h");
+    lib.installHeader("sds.h", "sds.h");
     lib.installHeader("alloc.h", "alloc.h");
     b.installArtifact(lib);
 }
